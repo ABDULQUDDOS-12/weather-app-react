b4486425b0750c1bb9e508fec1a5ca937b0dbe67
Abdul quddos <abdulquddoshafiz@gmail.com>
2023-05-27 21:51:58 +0500

    weather app

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import React from 'react'
 // import UseEffect from './components/hooks/useEffect'
 // import UseReducer from './components/hooks/useState'
 // import Todo from './components/todoReact/todo'
-import Form from './components/formValidation/form'
-// import Temp from './components/weather/temp'
+// import Form from './components/formValidation/form'
+import Temp from './components/weather/temp'
 const App = () => {
   return (
    
     <div>
-      <Form/>
-      {/* <Temp /> */}
+       {/* <Form/> */}
+      <Temp />
       {/* <Todo/> */}
       {/* <UseEffect/> */}
        {/* <UseState/> */}
